a0f394e8aecdebd9d37cf0d539d501c6cfd14c88
Joao Victor Soares Souza <1568660@sga.pucminas.br>
2025-02-28 10:25:03 -0300

    Questão 3

---
+++ Questão 3/Q3.cs
@@ -0,0 +1,27 @@
+﻿/* Lista2_AED - Questão3 - João Victor Soares Souza */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questão_3
+{
+    internal class Q3
+    {
+        static void Binário(int x)
+        {
+            int rest;
+            if (x >= 1)
+            {
+                Binário(x / 2);
+                rest = x % 2;
+                Console.Write(rest);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Binário(10);
+        }
+    }
+}
